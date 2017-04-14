6e4ce9ee553258065020897daaadeb8665862f5c
Ana Mercedes Gauna <amgauna@gmail.com>
2017-04-14 01:08:26 -0300

    Add files via upload

---
+++ ml.js
@@ -0,0 +1,7 @@
+function em_click(){
+location.href="mailto:amgauna@gmail.com";
+return true; }
+function ml_write(){
+document.write('amgauna@gmail.com');}
+function ml_hp_write(isrc, iwidth, iheight){
+document.write('<img title="amgauna@gmail.com" '+isrc+' '+iwidth+' '+iheight+' border=0>' );}
