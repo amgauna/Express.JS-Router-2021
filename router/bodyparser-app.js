7f1e4e823de4b1d9801a66a22743e03a9c6558f0
Ana Gauna <amgauna@gmail.com>
2021-12-06 12:06:45 -0300

    Update bodyparser-app.js

--- router/bodyparser-app.js
+++ router/bodyparser-app.js
@@ -3,10 +3,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+var urlencode = require('urlencode');
+var json = require('json-middleware');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
 const app = express();
 
+app.use(json);
+app.use(urlencode);
+app.use('/url/that/accepts/form-data', multipartMiddleware);
 app.use(bodyParser.json()); // add a middleware (so that express can parse request.body's json)
-
 app.use(express.bodyParser());
 
 app.post('/api/courses', (request, response) => {
