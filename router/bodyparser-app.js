61404661555c74d982747127619b8b5e2f469efb
Ana Gauna <amgauna@gmail.com>
2021-12-06 12:13:08 -0300

    Update bodyparser-app.js

--- router/bodyparser-app.js
+++ router/bodyparser-app.js
@@ -10,13 +10,13 @@ var multipartMiddleware = multipart();
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.json()); 
+app.use(express.urlencoded()); 
 app.use(express.bodyParser());
 
-app.use(json);
-app.use(urlencode);
-app.use(multer({ dest: './uploads/' }));
+app.use(json); // connect.json() handles application/json
+app.use(urlencode); // connect.urlencoded() handles application/x-www-form-urlencoded
+app.use(multer({ dest: './uploads/' })); // // multer() handles multipart/form-data
 app.use(bodyParser.json()); // add a middleware (so that express can parse request.body's json)
 app.use('/url/that/accepts/form-data', multipartMiddleware);
 
