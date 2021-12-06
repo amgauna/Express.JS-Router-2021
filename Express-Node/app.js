b0c9a6e4b46fbd152e92b0aa988663b93f67b7b4
Ana Gauna <amgauna@gmail.com>
2021-12-06 13:16:25 -0300

    Update app.js

--- Express-Node/app.js
+++ Express-Node/app.js
@@ -1,6 +1,7 @@
 // Importando e criando módulos
 var express = require('express');
 var app = express();
+var router = express.Router();
 
 app.get('/', function(req, res) {
   res.send('Olá Mundo!');
