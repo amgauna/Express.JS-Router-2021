cec43febc7afb747d72dffd85faf4480226d8100
Ana Gauna <amgauna@gmail.com>
2021-12-06 13:55:59 -0300

    Update app7.js

--- Express-Node/app7.js
+++ Express-Node/app7.js
@@ -5,10 +5,27 @@ componentDidMount() {
       .catch(err => console.log(err));
   }
 
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
+    // From the client side:
+    // Fetches our GET route from the Express server. 
+    // (Note the route we are fetching matches the GET route from server.js
+
+    callBackendAPI = async() => {
+    const response = await fetch('/express_backend/'  + iAmTheWordId, {method: 'POST', body: JSON.stringify(myData)} );                                               
     const body = await response.json();
+        
+    app.post('/express_backend/:word_id', (req, res) => {
+    //console.log(req.params)
+    res.send('success')
+    var word_id = req.params.word_id;
+    console.log(results)
+    return results; }
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+console.log(body);
+    return body;
+  };         
 
     if (response.status !== 200) {
       throw Error(body.message) 
